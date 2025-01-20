35c1eb6cc2d84bd656e7d1b5aa6e8925f15d01a5
jsm <64121719+mimimido12@users.noreply.github.com>
2025-01-21 03:04:32 +0900

    개인정보출력

--- function/src/main/kotlin/dev/study/개인정보출력.kt
+++ function/src/main/kotlin/dev/study/개인정보출력.kt
@@ -7,6 +7,17 @@ package dev.study
     모든 파라미터는 Nullable한 형태입니다.
     파라미터 값이 존재하지 않는다면 그 값을 제외하고 아래 형식대로 출력 하셔야합니다.
  */
+
+
+fun formatPersonDisplayAnswer(
+    name: String? = null,
+    surname: String? = null,
+    age: Int? = null,
+): String {
+    return "${name?:""} ${surname?:""} ${age?:""}"
+}
+
+
 fun main(){
     println(formatPersonDisplayAnswer("John", "Smith", 42))
     // John, Smith, 42
